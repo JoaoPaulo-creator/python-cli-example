ad065ca4818131f22e129d68c0c6dba3ea323eaf
JoaoPaulo-creator <joaosarto@gmail.com>
2023-08-01 21:34:23 -0300

    adicionado um exemplo de como lidar com excecoes

--- teste-falha.py
+++ teste-falha.py
@@ -1,28 +1,37 @@
-from httpx import post
+import traceback
+
+from httpx import post, get
 import typer
+import rich
 
 app = typer.Typer()
 
 
 def run_teste():
-    request = post("localhost:3000", json={
-        "teste": "teste"
-    })
-
-    return request
-
+    t = get("https://cep.awesomeapi.com.br/json/80010012").json()
+    # try:
+    # except Exception:
+    #     with open("error.txt", 'w') as file:
+    #         traceback_str = traceback.format_exc()
+    #         file.write(f'{traceback_str}')
+    #         file.close()
+    #
+    #     read_error_instruction = 'Execute o comando cat error.txt (mac e linux) ou more error.txt (windows)'
+    #
+    #     rich.print({
+    #         "message": f"Um erro ocorreu ao tentar rodar a requisicao. {read_error_instruction}"
+    #     })
+    #
+    return t
 
 @app.command()
 def should_create_proposal(
         approve: bool = False
 ):
-    try:
-        if approve:
-            run_teste()
-    except:
-        print("Um erro ocorreu ao tentar rodar a requisicao")
+    if approve:
+        rich.print(run_teste())
 
 
 if __name__ == "__main__":
     app()
-    #aise SystemExit
+    # aise SystemExit
